cc716bec5fb69ce446128d7febae32d465622c63
Andrea <andii.candii10@gmail.com>
2024-11-18 20:07:57 -0600

    primer commit

---
+++ main.js
@@ -0,0 +1,34 @@
+console.log (typeof "hola")
+// quiero ser un comentario 
+// hola q ase
+// holi 
+let nombre = ""
+
+if(nombre==="Juan"){console.log("Hola, Juan")}
+
+if (nombre === "Juan") {
+
+    console.log("Hola, Juan");
+
+}
+
+console.log(typeof "Hola")
+
+console.log(typeof 6)
+
+console.log(typeof 0)
+
+console.log(typeof true)
+
+console.log(typeof false)
+
+console.log(typeof TRUE)
+
+console.log(typeof "true")
+
+console.log(typeof 18/11/24)
+
+
+
+
+
